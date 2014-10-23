08c8b3a4f36c23ce5464be77a213cb9402541d3c
meissner.t <meissner.t@googlemail.com>
2014-10-23 15:28:11 -0700

    new plugins for QC3 and SNAP

---
+++ qc3_1_22.py
@@ -0,0 +1,18 @@
+from starcluster.clustersetup import ClusterSetup
+from starcluster.logger import log
+
+class QC3Installer(ClusterSetup):
+	def run(self, nodes, master, user, user_shell, volumes):
+		for node in nodes:
+			log.info("Installing QC3 1.22 on %s" % (node.alias))
+			node.ssh.execute('mkdir -p /opt/software/qc3/1.22')
+			node.ssh.execute('cd /opt/software/qc3/1.22 && git clone https://github.com/slzhao/QC3.git')
+			node.ssh.execute('cd /opt/software/qc3/1.22/QC3 && git checkout e44b5a38d1544a862365cb319bb678773c2f254e')
+			node.ssh.execute('chmod +x /opt/software/qc3/1.22/QC3/qc3.pl')
+
+			node.ssh.execute('mkdir -p /usr/local/Modules/applications/qc3/;touch /usr/local/Modules/applications/qc3/1.22')
+			node.ssh.execute('echo "#%Module" >> /usr/local/Modules/applications/qc3/1.22')
+			node.ssh.execute('echo "set root /opt/software/qc3/1.22/QC3" >> /usr/local/Modules/applications/qc3/1.22')
+			node.ssh.execute('echo -e "prepend-path\tPATH\t\$root" >> /usr/local/Modules/applications/qc3/1.22')
+			node.ssh.execute('echo module load R >> /usr/local/Modules/applications/qc3/1.22')
+			node.ssh.execute('echo module load samtools >> /usr/local/Modules/applications/qc3/1.22')
---
+++ snap_1_0_b13.py
@@ -0,0 +1,14 @@
+from starcluster.clustersetup import ClusterSetup
+from starcluster.logger import log
+
+class SNAPInstaller(ClusterSetup):
+	def run(self, nodes, master, user, user_shell, volumes):
+		for node in nodes:
+			log.info("Installing SNAP 1.0 beta 13 on %s" % (node.alias))
+			node.ssh.execute('wget -c -P /opt/software/snap http://snap.cs.berkeley.edu/downloads/snap-1.0beta.13-linux.tar.gz')
+			node.ssh.execute('tar -xvzf /opt/software/snap/snap-1.0beta.13-linux.tar.gz -C /opt/software/snap')
+
+			node.ssh.execute('mkdir -p /usr/local/Modules/applications/snap/;touch /usr/local/Modules/applications/snap/1.0.b13')
+			node.ssh.execute('echo "#%Module" >> /usr/local/Modules/applications/snap/1.0.b13')
+			node.ssh.execute('echo "set root /opt/software/snap/snap-1.0beta.13-linux" >> /usr/local/Modules/applications/snap/1.0.b13')
+			node.ssh.execute('echo -e "prepend-path\tPATH\t\$root" >> /usr/local/Modules/applications/snap/1.0.b13')
