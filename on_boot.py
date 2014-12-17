36f4dadad0b42593e0e515de1167cba50b36aa61
meissner.t <meissner.t@googlemail.com>
2014-12-17 13:48:11 -0800

    mount s3 buckets directly to each node

--- on_boot.py
+++ on_boot.py
@@ -26,16 +26,29 @@ class Setup(ClusterSetup):
         # node.ssh.execute('mount -t nfs master:/data/storage /data/storage')
 
         # add manually mounted s3 basespacebackup bucket
-        master.ssh.execute('echo "/data/s3/basespacebackup" %s"(async,no_root_squash,no_subtree_check,rw,fsid=0)" >> /etc/exports' % (node.alias))
-        master.ssh.execute("awk '!a[$0]++' /etc/exports | sponge /etc/exports") # get rid of duplicate entries 
-        master.ssh.execute('exportfs -a')
-        node.ssh.execute('mkdir -p /data/s3/basespacebackup')
-        node.ssh.execute('if mount | grep /data/s3/basespacebackup; then echo "already mounted"; else mount -t nfs master:/data/s3/basespacebackup /data/s3/basespacebackup; fi')
+        # master.ssh.execute('echo "/data/s3/basespacebackup" %s"(async,no_root_squash,no_subtree_check,rw,fsid=0)" >> /etc/exports' % (node.alias))
+        # master.ssh.execute("awk '!a[$0]++' /etc/exports | sponge /etc/exports") # get rid of duplicate entries 
+        # master.ssh.execute('exportfs -a')
+        # node.ssh.execute('mkdir -p /data/s3/basespacebackup')
+        # node.ssh.execute('if mount | grep /data/s3/basespacebackup; then echo "already mounted"; else mount -t nfs master:/data/s3/basespacebackup /data/s3/basespacebackup; fi')
 
-        # add authorized users for passwordless ssh login
-        # master.ssh.execute('for i in $(ls /home); do scp /home/$i/.ssh/authorized_keys %s:/home/$i/.ssh; done' % (node.alias))
-        # master.ssh.execute('for i in $(ls /home); do scp /home/$i/.ssh/id_rsa.pub %s:/home/$i/.ssh; done' % (node.alias))
-        # master.ssh.execute('for i in $(ls /home); do scp /home/$i/.ssh/id_rsa %s:/home/$i/.ssh; done' % (node.alias))
+        # master.ssh.execute('echo "/data/s3/averapatients" %s"(async,no_root_squash,no_subtree_check,rw,fsid=0)" >> /etc/exports' % (node.alias))
+        # master.ssh.execute("awk '!a[$0]++' /etc/exports | sponge /etc/exports") # get rid of duplicate entries 
+        # master.ssh.execute('exportfs -a')
+        # node.ssh.execute('mkdir -p /data/s3/averapatients')
+        # node.ssh.execute('if mount | grep /data/s3/averapatients; then echo "already mounted"; else mount -t nfs master:/data/s3/averapatients /data/s3/averapatients; fi')
+        
+        # master.ssh.execute('echo "/data/s3/averaprojects" %s"(async,no_root_squash,no_subtree_check,rw,fsid=0)" >> /etc/exports' % (node.alias))
+        # master.ssh.execute("awk '!a[$0]++' /etc/exports | sponge /etc/exports") # get rid of duplicate entries 
+        # master.ssh.execute('exportfs -a')
+        # node.ssh.execute('mkdir -p /data/s3/averaprojects')
+        # node.ssh.execute('if mount | grep /data/s3/averaprojects; then echo "already mounted"; else mount -t nfs master:/data/s3/averaprojects /data/s3/averaprojects; fi')
+
+        # master.ssh.execute('echo "/data/s3/averafastq" %s"(async,no_root_squash,no_subtree_check,rw,fsid=0)" >> /etc/exports' % (node.alias))
+        # master.ssh.execute("awk '!a[$0]++' /etc/exports | sponge /etc/exports") # get rid of duplicate entries 
+        # master.ssh.execute('exportfs -a')
+        # node.ssh.execute('mkdir -p /data/s3/averafastq')
+        # node.ssh.execute('if mount | grep /data/s3/averafastq; then echo "already mounted"; else mount -t nfs master:/data/s3/averafastq /data/s3/averafastq; fi')
 
         # sync node with headnode
         log.info('Syncing software with master node...')
@@ -49,3 +62,20 @@ class Setup(ClusterSetup):
         log.info("Install Ganglia monitoring")
         master.ssh.execute('scp /gmond.conf %s:/etc/ganglia/gmond.conf' % (node.alias))
         node.ssh.execute('service ganglia-monitor restart')
+
+        # remount /tmp to /mnt/tmp
+        log.info("Remounting /tmp to /mnt/tmp ..")
+        node.ssh.execute('mkdir -p -m 1777 /mnt/tmp')
+        node.ssh.execute('mount --bind /mnt/tmp /tmp')
+
+        # mount s3 buckets
+        log.info("Mounting S3 buckets ..")
+        node.ssh.execute('mkdir -p /data/s3/basespacebackup')
+        node.ssh.execute('mkdir -p /data/s3/averapatients')
+        node.ssh.execute('mkdir -p /data/s3/averaprojects')
+        node.ssh.execute('mkdir -p /data/s3/averafastq')
+
+        node.ssh.execute('if mount | grep /data/s3/basespacebackup; then umount -l /data/s3/basespacebackup && s3fs averafastq /data/s3/basespacebackup -o allow_other,uid=1002,gid=100,umask=0002,use_cache=/tmp; else s3fs basespacebackup /data/s3/basespacebackup -o allow_other,uid=1002,gid=100,umask=0002,use_cache=/tmp; fi')
+        node.ssh.execute('if mount | grep /data/s3/averapatients; then umount -l /data/s3/averapatients && s3fs averapatients /data/s3/averapatients -o allow_other,uid=1002,gid=100,umask=0002,use_cache=/tmp; else s3fs averapatients /data/s3/averapatients -o allow_other,uid=1002,gid=100,umask=0002,use_cache=/tmp; fi')
+        node.ssh.execute('if mount | grep /data/s3/averaprojects; then umount -l /data/s3/averaprojects && s3fs averaprojects /data/s3/averaprojects -o allow_other,uid=1002,gid=100,umask=0002,use_cache=/tmp; else s3fs averaprojects /data/s3/averaprojects -o allow_other,uid=1002,gid=100,umask=0002,use_cache=/tmp; fi')
+        node.ssh.execute('if mount | grep /data/s3/averafastq; then umount -l /data/s3/averafastq && s3fs averafastq /data/s3/averafastq -o allow_other,uid=1002,gid=100,umask=0002,use_cache=/tmp; else s3fs averafastq /data/s3/averafastq -o allow_other,uid=1002,gid=100,umask=0002,use_cache=/tmp; fi')
